5fbe58078e149ac882c4463e384460079e24aa3d
marufc36 <151602012+marufc36@users.noreply.github.com>
2025-01-10 18:45:38 +0600

    Update MarketPageStep.js

--- Steps/MarketPageStep.js
+++ Steps/MarketPageStep.js
@@ -31,6 +31,39 @@ class MarketPageStep {
         await this.marketPage.selectRarityCommon(rarityOption);
         await this.marketPage.clickSearchButton();
     }
+
+    async interactWithAdvancedSearchFor3rdTest(dota2Option, heroOption, rarityOption) {
+        await this.marketPage.clickAdvancedSearchButton();
+        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
+        assert(isFormDisplayed, "Advanced search form is not displayed");
+
+        await this.marketPage.selectDota2(dota2Option);
+        await this.marketPage.selectHeroInvoker(heroOption);
+        await this.marketPage.selectRarityMythical(rarityOption);
+        await this.marketPage.clickSearchButton();
+    }
+
+    async interactWithAdvancedSearchFor4thTest(dota2Option, heroOption, rarityOption) {
+        await this.marketPage.clickAdvancedSearchButton();
+        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
+        assert(isFormDisplayed, "Advanced search form is not displayed");
+
+        await this.marketPage.selectDota2(dota2Option);
+        await this.marketPage.selectHeroOracle(heroOption);
+        await this.marketPage.selectRarityImmortal(rarityOption);
+        await this.marketPage.clickSearchButton();
+    }
+
+    async interactWithAdvancedSearchFor5thTest(dota2Option, heroOption, rarityOption) {
+        await this.marketPage.clickAdvancedSearchButton();
+        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
+        assert(isFormDisplayed, "Advanced search form is not displayed");
+
+        await this.marketPage.selectDota2(dota2Option);
+        await this.marketPage.selectHeroAbandon(heroOption);
+        await this.marketPage.selectRarityAncient(rarityOption);
+        await this.marketPage.clickSearchButton();
+    }
 }
 
 module.exports = MarketPageStep;
