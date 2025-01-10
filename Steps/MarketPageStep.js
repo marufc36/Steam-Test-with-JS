c9231803628dccdc726cdb4ee929cc136f8eef67
marufc36 <151602012+marufc36@users.noreply.github.com>
2025-01-10 16:10:07 +0600

    Update MarketPageStep.js

--- Steps/MarketPageStep.js
+++ Steps/MarketPageStep.js
@@ -16,8 +16,19 @@ class MarketPageStep {
         assert(isFormDisplayed, "Advanced search form is not displayed");
 
         await this.marketPage.selectDota2(dota2Option);
-        await this.marketPage.selectHero(heroOption);
-        await this.marketPage.selectRarity(rarityOption);
+        await this.marketPage.selectHeroPhantomAssasin(heroOption);
+        await this.marketPage.selectRarityRare(rarityOption);
+        await this.marketPage.clickSearchButton();
+    }
+
+    async interactWithAdvancedSearchFor2ndTest(dota2Option, heroOption, rarityOption) {
+        await this.marketPage.clickAdvancedSearchButton();
+        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
+        assert(isFormDisplayed, "Advanced search form is not displayed");
+
+        await this.marketPage.selectDota2(dota2Option);
+        await this.marketPage.selectHeroLina(heroOption);
+        await this.marketPage.selectRarityCommon(rarityOption);
         await this.marketPage.clickSearchButton();
     }
 }
