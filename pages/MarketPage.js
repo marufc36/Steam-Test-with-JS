9b974b92c16a2e38cf344f0eedb5d3029a977bd5
marufc36 <151602012+marufc36@users.noreply.github.com>
2025-01-10 18:44:53 +0600

    Update MarketPage.js

--- pages/MarketPage.js
+++ pages/MarketPage.js
@@ -8,8 +8,14 @@ class MarketPage {
         this.dota2Option = By.xpath("//div[contains(@class,'popup_item') and contains(@id,'app_option_570')]");
         this.heroOptionPhantomAssasin =  By.xpath("//option[contains(@value,'dota_hero_phantom_assassin')]");
         this.heroOptionLina =  By.xpath("//option[contains(@value,'hero_lina')]");
+        this.heroOptionInvoker = By.xpath("(//option[contains(@value,'hero_invoker')])[1]");
+        this.heroOptionOracle = By.xpath("//option[contains(@value,'hero_oracle')]");
+        this.heroOptionAbandon = By.xpath("//option[contains(@value,'hero_abaddon')]");
         this.rarityOptionRare =  By.xpath("//input[contains(@id,'Rarity_Rare')]");
         this.rarityOptionCommon =  By.xpath("//input[contains(@id,'Rarity_Common')]");
+        this.rarityOptionMythical = By.xpath("//input[contains(@id,'Rarity_Mythical')]");
+        this.rarityImmortal = By.xpath("//input[contains(@id,'Rarity_Immortal')]");
+        this.rarityAncient = By.xpath("//input[contains(@id,'Rarity_Ancient')]");
         this.searchButton = By.xpath("//div[contains(@class,'btn_green')]");
         
     }
@@ -45,6 +51,21 @@ class MarketPage {
         await hero.click();
     }
 
+    async selectHeroInvoker() {
+        const hero = await this.driver.wait(until.elementLocated(this.heroOptionInvoker), 10000);
+        await hero.click();
+    }
+
+    async selectHeroOracle() {
+        const hero = await this.driver.wait(until.elementLocated(this.heroOptionOracle), 10000);
+        await hero.click();
+    }
+
+    async selectHeroAbandon() {
+        const hero = await this.driver.wait(until.elementLocated(this.heroOptionAbandon), 10000);
+        await hero.click();
+    }
+
     async selectRarityRare() {
         const rarity = await this.driver.findElement(this.rarityOptionRare);
         await rarity.click();
@@ -52,6 +73,21 @@ class MarketPage {
 
     async selectRarityCommon() {
         const rarity = await this.driver.findElement(this.rarityOptionCommon);
+        await rarity.click();
+    }
+
+    async selectRarityMythical() {
+        const rarity = await this.driver.findElement(this.rarityOptionMythical);
+        await rarity.click();
+    }
+
+    async selectRarityImmortal() {
+        const rarity = await this.driver.findElement(this.rarityImmortal);
+        await rarity.click();  
+    }
+
+    async selectRarityAncient() {
+        const rarity = await this.driver.findElement(this.rarityAncient);
         await rarity.click();
     }
 
@@ -67,3 +103,4 @@ class MarketPage {
 }
 
 module.exports = MarketPage;
+
