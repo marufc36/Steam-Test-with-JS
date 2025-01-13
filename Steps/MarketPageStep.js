9010c76dcc18fae1da798eadbc41d4cf661c8031
marufc36 <151602012+marufc36@users.noreply.github.com>
2025-01-13 11:31:00 +0600

    Update MarketPageStep.js

--- Steps/MarketPageStep.js
+++ Steps/MarketPageStep.js
@@ -10,60 +10,56 @@ class MarketPageStep {
         assert.equal(actualTitle, expectedTitle, "Market page title does not match");
     }
 
-    async interactWithAdvancedSearch(dota2Option, heroOption, rarityOption) {
+    async interactWithAdvancedSearch(heroOption, rarityOption) {
         await this.marketPage.clickAdvancedSearchButton();
         const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
         assert(isFormDisplayed, "Advanced search form is not displayed");
 
-        await this.marketPage.selectDota2(dota2Option);
-        await this.marketPage.selectHeroPhantomAssasin(heroOption);
-        await this.marketPage.selectRarityRare(rarityOption);
-        await this.marketPage.clickSearchButton();
-    }
+        await this.marketPage.selectDota2();
 
-    async interactWithAdvancedSearchFor2ndTest(dota2Option, heroOption, rarityOption) {
-        await this.marketPage.clickAdvancedSearchButton();
-        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
-        assert(isFormDisplayed, "Advanced search form is not displayed");
+        switch (heroOption) {
+            case "Phantom Assassin":
+                await this.marketPage.selectHeroPhantomAssasin();
+                break;
+            case "Lina":
+                await this.marketPage.selectHeroLina();
+                break;
+            case "Invoker":
+                await this.marketPage.selectHeroInvoker();
+                break;
+            case "Oracle":
+                await this.marketPage.selectHeroOracle();
+                break;
+            case "Abaddon":
+                await this.marketPage.selectHeroAbandon();
+                break;
+            default:
+                throw new Error(`Unknown hero option: ${heroOption}`);
+        }
 
-        await this.marketPage.selectDota2(dota2Option);
-        await this.marketPage.selectHeroLina(heroOption);
-        await this.marketPage.selectRarityCommon(rarityOption);
-        await this.marketPage.clickSearchButton();
-    }
+        switch (rarityOption) {
+            case "Rare":
+                await this.marketPage.selectRarityRare();
+                break;
+            case "Common":
+                await this.marketPage.selectRarityCommon();
+                break;
+            case "Mythical":
+                await this.marketPage.selectRarityMythical();
+                break;
+            case "Immortal":
+                await this.marketPage.selectRarityImmortal();
+                break;
+            case "Ancient":
+                await this.marketPage.selectRarityAncient();
+                break;
+            default:
+                throw new Error(`Unknown rarity option: ${rarityOption}`);
+        }
 
-    async interactWithAdvancedSearchFor3rdTest(dota2Option, heroOption, rarityOption) {
-        await this.marketPage.clickAdvancedSearchButton();
-        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
-        assert(isFormDisplayed, "Advanced search form is not displayed");
-
-        await this.marketPage.selectDota2(dota2Option);
-        await this.marketPage.selectHeroInvoker(heroOption);
-        await this.marketPage.selectRarityMythical(rarityOption);
-        await this.marketPage.clickSearchButton();
-    }
-
-    async interactWithAdvancedSearchFor4thTest(dota2Option, heroOption, rarityOption) {
-        await this.marketPage.clickAdvancedSearchButton();
-        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
-        assert(isFormDisplayed, "Advanced search form is not displayed");
-
-        await this.marketPage.selectDota2(dota2Option);
-        await this.marketPage.selectHeroOracle(heroOption);
-        await this.marketPage.selectRarityImmortal(rarityOption);
-        await this.marketPage.clickSearchButton();
-    }
-
-    async interactWithAdvancedSearchFor5thTest(dota2Option, heroOption, rarityOption) {
-        await this.marketPage.clickAdvancedSearchButton();
-        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
-        assert(isFormDisplayed, "Advanced search form is not displayed");
-
-        await this.marketPage.selectDota2(dota2Option);
-        await this.marketPage.selectHeroAbandon(heroOption);
-        await this.marketPage.selectRarityAncient(rarityOption);
         await this.marketPage.clickSearchButton();
     }
 }
 
 module.exports = MarketPageStep;
+
