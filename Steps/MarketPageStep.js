0abadfcc6d6b15eb72228384f88ce3f67cbe15e9
marufc36 <151602012+marufc36@users.noreply.github.com>
2025-01-14 12:53:44 +0600

    Update MarketPageStep.js

--- Steps/MarketPageStep.js
+++ Steps/MarketPageStep.js
@@ -16,50 +16,24 @@ class MarketPageStep {
         assert(isFormDisplayed, "Advanced search form is not displayed");
 
         await this.marketPage.selectDota2();
-
-        switch (heroOption) {
-            case "Phantom Assassin":
-                await this.marketPage.selectHeroPhantomAssasin();
-                break;
-            case "Lina":
-                await this.marketPage.selectHeroLina();
-                break;
-            case "Invoker":
-                await this.marketPage.selectHeroInvoker();
-                break;
-            case "Oracle":
-                await this.marketPage.selectHeroOracle();
-                break;
-            case "Abaddon":
-                await this.marketPage.selectHeroAbandon();
-                break;
-            default:
-                throw new Error(`Unknown hero option: ${heroOption}`);
-        }
-
-        switch (rarityOption) {
-            case "Rare":
-                await this.marketPage.selectRarityRare();
-                break;
-            case "Common":
-                await this.marketPage.selectRarityCommon();
-                break;
-            case "Mythical":
-                await this.marketPage.selectRarityMythical();
-                break;
-            case "Immortal":
-                await this.marketPage.selectRarityImmortal();
-                break;
-            case "Ancient":
-                await this.marketPage.selectRarityAncient();
-                break;
-            default:
-                throw new Error(`Unknown rarity option: ${rarityOption}`);
-        }
-
+        await this.marketPage.selectHero(heroOption);
+        await this.marketPage.selectRarity(rarityOption);
         await this.marketPage.clickSearchButton();
     }
+
+    async interactWithAdvancedSearchForCS2() {
+        await this.marketPage.clickAdvancedSearchButton();
+        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
+        assert(isFormDisplayed, "Advanced search form is not displayed");
+    
+        await this.marketPage.selectCounterStrike();
+        await this.marketPage.selectWeapon();
+        await this.marketPage.selectExterior();
+        await this.marketPage.clickSearchButton();
+    }
+    
 }
 
 module.exports = MarketPageStep;
+
 
