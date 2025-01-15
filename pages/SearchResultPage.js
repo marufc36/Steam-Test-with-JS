5dda3009483dc1f509cc2493b104cdeeeb48a3c6
marufc36 <151602012+marufc36@users.noreply.github.com>
2025-01-15 21:50:03 +0600

    Implement new test cases.

--- pages/SearchResultPage.js
+++ pages/SearchResultPage.js
@@ -5,6 +5,7 @@ class SearchResultPage {
         this.driver = driver;
         this.searchResults = By.xpath("//div[@id='searchResults']");
         this.firstItem = By.xpath("//div[@id='result_0']");
+        this.fourthItem = By.xpath("//div[@id='result_3']");
         this.gameNameLocator = By.xpath("//div[@id='largeiteminfo_game_name']");
         this.rareity = By.xpath("//div[@id='largeiteminfo_item_type']");
 
@@ -20,18 +21,24 @@ class SearchResultPage {
         await item.click();
     }
 
-    async verifyGameName(expectedGameName) {
+    async clickFourthItem() {
+        const item = await this.driver.findElement(this.fourthItem);
+        await item.click();
+    }
+
+    async gameName(expectedGameName) {
         const gameNameElement = await this.driver.wait(until.elementLocated(this.gameNameLocator), 10000);
         const actualGameName = await gameNameElement.getText();
         return actualGameName === expectedGameName;
     }
 
-    async verifyrarity(expectedRareity) {
+    async rarity(expectedRareity) {
         const elementRarity = await this.driver.wait(until.elementLocated(this.rareity), 10000);
         const actualGameName = await elementRarity.getText();
         return actualGameName.includes(expectedRareity);
     }
 }
 module.exports = SearchResultPage;
+
 
 
