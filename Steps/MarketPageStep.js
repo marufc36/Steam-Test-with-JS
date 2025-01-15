f105198a11ea10575158d33b29ca08ca24bcdefa
marufc36 <151602012+marufc36@users.noreply.github.com>
2025-01-15 21:51:16 +0600

    Update MarketPageStep.js with new hero , quality , rarity

--- Steps/MarketPageStep.js
+++ Steps/MarketPageStep.js
@@ -1,39 +1,20 @@
 const assert = require("assert");
 
-class MarketPageStep {
-    constructor(marketPage) {
-        this.marketPage = marketPage;
+class HomePageStep {
+    constructor(homePage) {
+        this.homePage = homePage;
     }
 
-    async verifyMarketPageTitle(expectedTitle) {
-        const actualTitle = await this.marketPage.getTitle();
-        assert.equal(actualTitle, expectedTitle, "Market page title does not match");
+    async openHomePageAndVerifyTitle(expectedTitle) {
+        await this.homePage.open();
+        const actualTitle = await this.homePage.getTitle();
+        assert.equal(actualTitle, expectedTitle, "Home page title does not match");
     }
 
-    async interactWithAdvancedSearch(heroOption, rarityOption) {
-        await this.marketPage.clickAdvancedSearchButton();
-        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
-        assert(isFormDisplayed, "Advanced search form is not displayed");
-
-        await this.marketPage.selectDota2();
-        await this.marketPage.selectHero(heroOption);
-        await this.marketPage.selectRarity(rarityOption);
-        await this.marketPage.clickSearchButton();
+    async navigateToMarket() {
+        await this.homePage.hoverOverCommunityMenu();
+        await this.homePage.clickMarketLink();
     }
-
-    async interactWithAdvancedSearchForCS2() {
-        await this.marketPage.clickAdvancedSearchButton();
-        const isFormDisplayed = await this.marketPage.isAdvancedSearchFormDisplayed();
-        assert(isFormDisplayed, "Advanced search form is not displayed");
-    
-        await this.marketPage.selectCounterStrike();
-        await this.marketPage.selectWeapon();
-        await this.marketPage.selectExterior();
-        await this.marketPage.clickSearchButton();
-    }
-    
 }
 
-module.exports = MarketPageStep;
-
-
+module.exports = HomePageStep;
